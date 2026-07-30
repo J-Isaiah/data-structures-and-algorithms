ccf64432a3e4a83d5a3494c4ed3f6c5f87393f99
Isaiah J <133605060+J-Isaiah@users.noreply.github.com>
2026-07-29 21:14:53 -0500

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        visited = set()
+        q = collections.deque([amount])
+        coins_used = 0
+        while q: 
+            level_size = len(q)
+            coins_used += 1
+            for l in range(level_size):
+                cur_amount = q.popleft()
+                for coin in coins:
+                    remainder = cur_amount - coin
+                    if remainder in visited:
+                        continue
+                        
+                    visited.add(remainder)
+                    
+                    if remainder > 0:
+                        q.append(remainder)
+                    if remainder == 0:
+                        return coins_used
+                        
+        
+        return -1
+                
+
+            
