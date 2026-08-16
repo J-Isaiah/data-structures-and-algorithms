4ff06c6bda802e7590658eb5995d9afbb17de9d6
Isaiah J <133605060+J-Isaiah@users.noreply.github.com>
2026-08-16 18:27:05 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val=None, neighbors=None, state = 0):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+        self.state = state
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Build a list
+
+        adjList = {}
+        for c1, c2 in prerequisites:
+            if c1 not in adjList:
+                adjList[c1] = Node(c1)
+            if c2 not in adjList:
+                adjList[c2] = Node(c2)
+
+            adjList[c2].neighbors.append(adjList[c1])
+
+
+        # Algorithem
+        def dfs(node):
+            if node.state == 2:
+                return True
+            node.state = 1
+            for neighbor in node.neighbors:
+                if neighbor.state == 1:
+                    return False
+                next = dfs(neighbor)
+
+                if next == False:
+                    return False
+
+            node.state=2
+            return True
+
+        for value in adjList.values():
+            if not dfs(value):
+                return False
+        
+        return True 
