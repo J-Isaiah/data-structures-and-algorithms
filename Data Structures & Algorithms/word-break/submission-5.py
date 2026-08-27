aae23125d3fd67d54a702008f3da2112584d75b9
Isaiah J <133605060+J-Isaiah@users.noreply.github.com>
2026-08-26 20:35:19 -0500

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        c = {}
+        def dp(idx):
+            if idx in c:
+                return c[idx]
+            if idx == len(s):
+                return True
+
+            for word in wordDict:
+                if s[idx : idx + len(word)] == word:
+                    branch = dp(idx + len(word))
+                    if not branch:
+                        continue 
+                    
+                    c[idx]=True
+
+                    return True
+
+
+                        
+            
+            c[idx]=False
+            return False
+        
+        return dp(0)
+                    
+
+        
